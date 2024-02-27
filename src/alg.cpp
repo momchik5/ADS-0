90ff312853836796c1802c3f0a31a7699a129496
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-02-27 15:43:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,29 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
 
+
+using namespace std;
+int gcd(int, int );
+int main() {
+	
+		std::cout << gcd(10, 15) << std::endl;
+		return 0;
+	
+		
+}
 int gcd(int a, int b) {
-    std::cout << gcd(10,15) << std::endl;
-    return 0;
-
+	int max = 0;
+	if (a>b) {
+		max=a;
+	}
+	else {
+		max = b;
+	}
+	int y = 0;
+	for (int i = 1; i < max; i++) {
+		if ((a % i == 0) && (b % i == 0)) {
+			y = i;
+		}
+		
+	}
+	return y;
 }
