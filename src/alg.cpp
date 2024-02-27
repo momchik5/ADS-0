9bdf8f2a050606cdcae53da0e40294dea107cefc
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-02-27 16:30:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-int gcd(int, int );
+#include "alg.h"
+
+int gcd(int, int);
 int main() {
- std::cout << gcd(10, 15) << std::endl;
- return 0;	
+    std::cout << gcd(10, 15) << std::endl;
+    return 0;
 }
 int gcd(int a, int b) {
- int max = 0;
- if (a>b) {
-  max=a;
- }
- else {
-  max = b;
-  }
- int y = 0;
- for (int i = 1; i < max; i++) {
-  if ((a % i == 0) && (b % i == 0)) {
-    y = i;
-   }
- }
- return y;
+    int max = 0;
+    if (a > b) {
+        max = a;
+    }
+    else {
+        max = b;
+    }
+    int y = 0;
+    for (int i = 1; i < max; i++) {
+        if ((a % i == 0) && (b % i == 0)) {
+            y = i;
+        }
+    }
+    return y;
 }
