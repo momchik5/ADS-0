70ebe672a60c5e4c4e61b46d8c2b8178fe256583
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-03-12 14:32:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,48 +1,11 @@
-// Copyright 2022 NNTU-CS
-#include <stdio.h>
-#include <cstdint>
-#include "alg.h"
+// Copyright 2021 NNTU-CS
 
-double pown(double value, uint16_t n) {
-    double r = 1.0;
-    for (uint16_t i = 0; i < n; ++i) {
-        r *= value;
+int cbinsearch(const int* arr, int size, int value) {
+  int quantity = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value) {
+      quantity++;
     }
-    return r;
-}
-
-uint64_t fact(uint16_t n) {
-    if (n <= 1) {
-        return 1;
-    } else {
-        return n * fact(n - 1);
-    }
-}
-
-double calcItem(double x, uint16_t n) {
-    return (pown(x, n) / fact(n));
-}
-
-double expn(double x, uint16_t count) {
-    double summa = 0.0;
-    for (uint16_t i = 0; i <= count; i++) {
-        summa += calcItem(x, i);
-    }
-    return summa;
-}
-
-double sinn(double x, uint16_t count) {
-    double res1 = 0.0;
-    for (uint16_t i = 1; i <= count; i++) {
-        res1 += pown(-1, i - 1) * calcItem(x, (2 * i) - 1);
-    }
-    return res1;
-}
-
-double cosn(double x, uint16_t count) {
-    double res2 = 0.0;
-    for (uint16_t i = 1; i <= count; i++) {
-        res2 += pown(-1, i - 1) * calcItem(x, 2 * i);
-    }
-    return res2;
+  }
+  return quantity;  // возвращает количество вхождений
 }
