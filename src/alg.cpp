70d92d15b29580379dd25db145fd87a19b5b6f46
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-03-19 22:37:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,55 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int* arr, int size, int value) {
-  int quantity = 0;
-  for (int i = 0; i < size; i++) {
-    if (arr[i] == value) {
-      quantity++;
+int countPairs1(int* arr, int kol, int val) {
+  int q = 0;
+  for (int i = 0; i < kol; i++) {
+    for (int j = i + 1; j < kol; j++) {
+      if (arr[i] + arr[j] == val) {
+        q++;
+      }
     }
   }
-  return quantity;
+  return q;
+}
+
+int countPairs2(int* arr, int kol, int val) {
+  int q = 0, pos = kol - 1;
+  for (int i = kol; i > 0; --i) {
+    if (arr[i] > val) {
+      pos = i;
+    }
+  }
+  for (int i = 0; i < pos; ++i) {
+    for (int j = pos; j > i; --j) {
+      if (arr[i] + arr[j] == val) {
+        q++;
+      }
+    }
+  }
+  return q;
+}
+
+int countPairs3(int* arr, int kol, int val) {
+  int q = 0;
+  for (int i = 0; i < kol - 1; i++) {
+    int left = i;
+    int right = kol;
+    while (left < right - 1) {
+      int mid = (right + left) / 2;
+      if (arr[i] + arr[mid] == val) {
+        q++;
+        int j = mid + 1;
+        while ((arr[i] + arr[j] == val) && (j < right)) {
+          q++;
+          j++;
+        }
+      }
+      if (arr[i] + arr[mid] < val) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+  }
+  return q;
 }
