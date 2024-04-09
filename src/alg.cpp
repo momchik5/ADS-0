2e71baaba7d238b1f8c7107c027dcfcf71b66007
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-10 02:16:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,4 @@
 // Copyright 2021 NNTU-CS
-// Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
@@ -7,55 +6,51 @@
 int prioritet(char);
 int schet(int, int, char);
 
-std::string infx2pstfx(std::string infix) {
-std::string postfix = "";
-TStack<char, 100> d;
-for (int i = 0; i < infix.length(); i++) {
-    if (isdigit(infix[i])) {
-        postfix += infix[i];
+std::string infx2pstfx(std::string inf) {
+    std::string postfix = "";
+    TStack<char, 100> d;
+    for (int i = 0; i < infix.length(); i++) {
+        if (isdigit(infix[i])) {
+            postfix += infix[i];
+            if (i != infix.length() - 1) {
+                postfix += " ";
+            }
+        } else if (infix[i] == '(') {
+            d.push(infix[i]);
+        } else if (d.isempty()) {
+            d.push(infix[i]);
+        } else if (infix[i] == ')') {
+            while (true) {
+                if (d.isempty()) {
+                    break;
+                } else if (d.check() == '(') {
+                    break;
+                }
+                postfix += d.pop();
+                if (i != infix.length() - 1) {
+                    postfix += " ";
+                }
+            }
+        }
+        d.pop();
+else if (prioritet(infix[i]) > prioritet(d.check())) {
+    d.push(infix[i]);
+}
+else {
+    while (true) {
+        if (d.isempty()) {
+            break;
+        }
+        else if (!(prioritet(infix[i]) <= prioritet(d.check()))) {
+            break;
+        }
+        postfix += d.pop();
         if (i != infix.length() - 1) {
             postfix += " ";
         }
     }
-        else if (infix[i] == '(') {
-        d.push(infix[i]);
-        }
-        else if (d.isempty()) {
-        d.push(infix[i]);
-        }
-        else if (infix[i] == ')') {
-        while (true) {
-            if (d.isempty()) {
-                break;
-            }
-            else if (d.check() == '(') {
-                break;
-            }
-            postfix += d.pop();
-            if (i != infix.length() - 1) {
-                postfix += " ";
-            }
-        }
-        d.pop();
-    }
-    else if (prioritet(infix[i]) > prioritet(d.check())) {
-        d.push(infix[i]);
-    }
-    else {
-        while (true) {
-            if (d.isempty()) {
-                break;
-            }
-            else if (!(prioritet(infix[i]) <= prioritet(d.check()))) {
-                break;
-            }
-            postfix += d.pop();
-            if (i != infix.length() - 1) {
-                postfix += " ";
-            }
-        }
-        d.push(infix[i]);
-    }
+    d.push(infix[i]);
+}
 }
 while (!d.isempty()) {
     postfix += " ";
@@ -113,5 +108,5 @@ int eval(std::string post) {
          return x / y;
      default:
          return 0;
-     }
- }
+  }
+}
