16cb12012098d4bff8f282015d975dfe633e63a2
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-10 01:51:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,55 +1,49 @@
 // Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-int countPairs1(int* arr, int kol, int val) {
-  int q = 0;
-  for (int i = 0; i < kol; i++) {
-    for (int j = i + 1; j < kol; j++) {
-      if (arr[i] + arr[j] == val) {
-        q++;
-      }
-    }
-  }
-  return q;
-}
+#include <iostream>
 
-int countPairs2(int* arr, int kol, int val) {
-  int q = 0, pos = kol - 1;
-  for (int i = kol; i > 0; --i) {
-    if (arr[i] > val) {
-      pos = i;
-    }
-  }
-  for (int i = 0; i < pos; ++i) {
-    for (int j = pos; j > i; --j) {
-      if (arr[i] + arr[j] == val) {
-        q++;
-      }
-    }
-  }
-  return q;
-}
+template<typename T, int size>
+class TStack {
+private:
+    T* array;
+    int top;
 
-int countPairs3(int* arr, int kol, int val) {
-  int q = 0;
-  for (int i = 0; i < kol - 1; i++) {
-    int left = i;
-    int right = kol;
-    while (left < right - 1) {
-      int mid = (right + left) / 2;
-      if (arr[i] + arr[mid] == val) {
-        q++;
-        int j = mid + 1;
-        while ((arr[i] + arr[j] == val) && (j < right)) {
-          q++;
-          j++;
+public:
+    TStack() : top(0) {
+        array = new T[size];
+    }
+    
+    T pop() {
+        if (top == 0) {
+            throw "Empty!";
+        } else {
+            top -= 1;
+            return array[top];
         }
-      }
-      if (arr[i] + arr[mid] < val) {
-        left = mid;
-      } else {
-        right = mid;
-      }
     }
-  }
-  return q;
-}
+
+    void push(T item) {
+        array[top] = item;
+        top++;
+    }
+
+    T get() const {
+        return array[top];
+    }
+
+    bool isempty() {
+        return top == 0;
+    }
+
+    T check() {
+        if (top == 0) {
+            throw "Empty!";
+        } else {
+            return array[top - 1];
+        }
+    }
+};
+
+#endif  // INCLUDE_TSTACK_H_ = pos; j > i; --j) {
