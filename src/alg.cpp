a606b0d06e1a3b48f2c844f219ffb666f528fd1a
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-10 02:03:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,49 +1,117 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+// Copyright 2021 NNTU-CS
+#include <string>
+#include <map>
+#include "tstack.h"
 
-#include <iostream>
+int prioritet(char);
+int schet(int, int, char);
 
-template<typename T, int size>
-class TStack {
- private:
-    T* array;
-    int top;
-
- public:
-    TStack() : top(0) {
-        array = new T[size];
-    }
-
-    T pop() {
-        if (top == 0) {
-            throw "Empty!";
-        } else {
-            top -= 1;
-            return array[top];
+std::string infx2pstfx(std::string infix) {
+std::string postfix = "";
+TStack<char, 100> d;
+for (int i = 0; i < infix.length(); i++) {
+    if (isdigit(infix[i])) {
+        postfix += infix[i];
+        if (i != infix.length() - 1) {
+            postfix += " ";
         }
     }
-
-    void push(T item) {
-        array[top] = item;
-        top++;
+        else if (infix[i] == '(') {
+        d.push(infix[i]);
+        }
+        else if (d.isempty()) {
+        d.push(infix[i]);
+        }
+        else if (infix[i] == ')') {
+        while (true) {
+            if (d.isempty()) {
+                break;
+            }
+            else if (d.check() == '(') {
+                break;
+            }
+            postfix += d.pop();
+            if (i != infix.length() - 1) {
+                postfix += " ";
+            }
+        }
+        d.pop();
     }
-
-    T get() const {
-        return array[top];
+    else if (prioritet(infix[i]) > prioritet(d.check())) {
+        d.push(infix[i]);
     }
-
-    bool isempty() {
-        return top == 0;
+    else {
+        while (true) {
+            if (d.isempty()) {
+                break;
+            }
+            else if (!(prioritet(infix[i]) <= prioritet(d.check()))) {
+                break;
+            }
+            postfix += d.pop();
+            if (i != infix.length() - 1) {
+                postfix += " ";
+            }
+        }
+        d.push(infix[i]);
     }
+}
+while (!d.isempty()) {
+    postfix += " ";
+    postfix += d.pop();
+}
+return postfix;
+}
 
-    T check() {
-        if (top == 0) {
-            throw "Empty!";
-        } else {
-            return array[top - 1];
+int eval(std::string post) {
+    std::string stroka = "";
+    TStack<int, 100> operand;
+
+    for (int i = 0; i < post.length(); i++) {
+        if (isdigit(post[i])) {
+            stroka += post[i];
+        }
+        else if (stroka.length() && post[i] == ' ') {
+            operand.push(atoi(stroka.c_str()));
+            stroka = "";
+        }
+        else {
+            int two = operand.pop();
+            int one = operand.pop();
+            operand.push(schet(one, two, post[i]));
         }
     }
-};
-
-#endif  // INCLUDE_TSTACK_H_ = pos; j > i; --j) {
+    return operand.pop();
+}
+ int prioritet(char c) {
+    if (c == '(') {
+        return 0;
+    }
+    else if (c == ')') {
+        return 1;
+    }
+    else if ((c == '+') || (c == '-')) {
+        return 2;
+    }
+    else if ((c == '*') || (c == '/')) {
+        return 3;
+    }
+    else {
+        throw "Error!";
+    }
+}
+ int schet(int x, int y, char znak) {
+     switch (znak) {
+     case '+':
+         return x + y;
+     case '-':
+         return x - y;
+     case '*':
+         return x * y;
+     case '/':
+         return x / y;
+     default:
+         return 0;
+     }
+ }
