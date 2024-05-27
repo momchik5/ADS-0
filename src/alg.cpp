46feb1557ef65d592f50b0508c78db149dec287e
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-05-28 02:12:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,76 +1,29 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-
 #include <iostream>
-#include <string>
-#include <algorithm>
-
-template <typename T>
-class BinarySearchTree {
- public:
-    struct TreeNode {
-        T value;
-        int count;
-        TreeNode* left;
-        TreeNode* right;
-    };
-
- private:
-    TreeNode* root;
-
-    TreeNode* insert(TreeNode* node, T value) {
-        if (node == nullptr) {
-            node = new TreeNode;
-            node->value = value;
-            node->count = 1;
-            node->left = node->right = nullptr;
-        } else if (value < node->value) {
-            node->left = insert(node->left, value);
-        } else if (value > node->value) {
-            node->right = insert(node->right, value);
-        } else {
-            node->count++;
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h
+BST<std::string> makeTree(const char* filename) {  
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return BST<std::string>();
+    }
+    char symbol;
+    std::string word;
+    BST<std::string> tree;
+    while (file.get(symbol)) {
+        if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
+            if (symbol >= 'A' && symbol <= 'Z') {
+                symbol = symbol + ('a' - 'A');
+            }
+            word += symbol;
+        } else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
         }
-        return node;
-    }
-
-    int calculateDepth(TreeNode* node) {
-        if (node == nullptr) {
-            return 0;
-        } else {
-            int leftDepth = calculateDepth(node->left);
-            int rightDepth = calculateDepth(node->right);
-            return std::max(leftDepth, rightDepth) + 1;
-        }
-    }
-
-    int find(TreeNode* node, T value) {
-        if (node == nullptr) {
-            return 0;
-        } else if (value == node->value) {
-            return node->count;
-        } else if (value < node->value) {
-            return find(node->left, value);
-        } else {
-            return find(node->right, value);
-        }
-    }
-
- public:
-    BinarySearchTree() : root(nullptr) {}
-
-    void add(T value) {
-        root = insert(root, value);
-    }
-
-    int depth() {
-        return calculateDepth(root) - 1;
-    }
-
-    int search(T value) {
-        return find(root, value);
-    }
-};
-
-#endif  // INCLUDE_BST_H_
+    } 
+    file.close();
+    return tree;
+}
